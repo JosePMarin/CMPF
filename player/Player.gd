8e7e04d485aebb658f3c397ae6e7f5b89a45c06f
Jose Pablo Marin Valera <elfindetusdias@gmail.com>
2019-08-25 16:35:13 +0200

    Starting health management

--- player/Player.gd
+++ player/Player.gd
@@ -6,28 +6,37 @@ extends KinematicBody2D
 const SPEED = 120
 const HIDE_SPEED = 40
 var DEATH=false
-var HEALTH = 100
+var HEALTH=100
+var STAMINA = 100
+var TIME = 0
+var HEALTH_REGEN=2
+var STAMINA_REGEN=2
+var damage=0
 
 #Objetos
 var movedir = Vector2()
 var spritedir = Vector2()
+var hurt_ref=hurt(damage)
+
 	
 #funcion que checkea el estado del personaje para mostrar animaciones
 func _ready():
 	pass
-	#TODO: state_machine = $AnimationTree.get(paremeters_player) TEST
+	#TODO: state_machine = $AnimationTree.get(paremeters_player)
 
 #funcion que controla el movimiento y las animaciones
 func _physics_process(delta):
-	_healthstate_loop()
-	_controls_loop()
+	TIME+=delta
 	_movement_loop()
+	_controls_loop()
+	_staminastate_loop()
+	_healthstate_loop(hurt_ref)
 	_spritedir_loop()
-	_animloader_loop()
+	_animloader_loop(delta)
 	#TODO: _spritestate_loop()
 
 #funcion que cambia la animacion en funcion del entorno
-func _animloader_loop():
+func _animloader_loop(delta):
 	if movedir != Vector2.ZERO:
 	 	if is_on_wall():
 	 		if test_move(transform, spritedir):
@@ -40,12 +49,16 @@ func _animloader_loop():
 #funcion que altera el movimiento en funcion de los inputs de teclado: proyecto/ajustes/mapa_de_entrada
 func _controls_loop():
 	movement()
+	#TODO: attak()
 
 #funcion que aplica el movimiento introducido (por _controls_loop()) y normalizado a la constante SPEED
 func _movement_loop():
 	var linear_velocity
 	var floor_normal = Vector2(0,0)
+	if is_on_wall():
+		hurt(50)
 	if input.hide():
+		hurt(20)
 		linear_velocity = movedir.normalized() * HIDE_SPEED
 	else:
 		linear_velocity = movedir.normalized() * SPEED
@@ -56,9 +69,33 @@ func _spritedir_loop():
 	if movedir != Vector2.ZERO && movedir != Vector2.INF:
 		spritedir = movedir
 
-func _healthstate_loop():
+#funcion que devueve el estado de stamina en funcion de delta(frames)
+func _staminastate_loop():
+	if STAMINA<100:
+		print ("regenerating stamina")
+		STAMINA+=STAMINA_REGEN
+	#print ("stamina= ", STAMINA)
+	return STAMINA
+
+#funcion que devuelve el estado de health en funcion de delta(frames)
+func _healthstate_loop(hurt_ref):
+
 	if HEALTH<=0:
+		print ("death")
 		DEATH=true
+		return false
+	else:
+		if HEALTH<100:
+			print ("regenerating health")
+			HEALTH+=HEALTH_REGEN
+			return HEALTH
+		print ("damage: ",int(hurt_ref))
+		if hurt_ref:
+			HEALTH-=int(hurt_ref)
+			print ("health after damage= ", HEALTH)
+			return HEALTH
+		print ("current health= ", HEALTH)
+	
 	
 
 
@@ -107,13 +144,22 @@ func movement():
 	movedir.x = -input.left() + input.right()
 	movedir.y = -input.up() + input.down()
 
-func hurt():
-	#TODO: boolean that returns true if damage
-	pass
+func hurt(damage):
+	var damage_dealt
+	if damage!=0:
+		damage_dealt=damage
+		damage=0
+		return damage_dealt
+	else:
+		return false
+
+func stamina():
+	return false
 	
 func die():
-	if DEATH:
+	if DEATH==true:
 		set_physics_process(false)
+		#todo: cargar animacion muerte
 		return true
 	return false
 
