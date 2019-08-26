3055e31eb70844f658baa0ec43e5cea51a861685
Juan Antonio <juanals85@gmail.com>
2019-08-26 17:48:09 +0200

    se cura y to

--- player/Player.gd
+++ player/Player.gd
@@ -20,13 +20,11 @@ var HEALTH_REGEN = 1
 var inicio_segundo=false
 var DEATH=false
 var delay=false
-var tired = false
+var TIRED = false
 
 #Variables status
-
 var damage = 0
 var damage_dealt = 0
-var delay_time = 0
 var stamina = 0
 var stamina_cost = 0
 
@@ -53,29 +51,71 @@ func _ready():
 #funcion que controla el movimiento y las animaciones
 func _physics_process(delta):
 	TIME+=delta
+	#START
 	_time()
+	_status_control()
 	_movement_loop()
 	_controls_loop()
-	_healthstate_loop(damage_dealt)
-	_staminastate_loop(stamina_cost)
 	_spritedir_loop()
 	_animloader_loop(delta)
-	_healthRegen_loop()
-	_staminaRegen_loop()
-	_delay()
-	_consolator_printer ()	
+	_consolator_printer ()
+	#TODO: _spritestate_loop()
+	#END
 	counterFps += 1
 	inicio_segundo = false
-	#TODO: _spritestate_loop()
-
-func _time():
-	if counterFps == fps:
-		_logger_ ("health: ", HEALTH)
-		_logger_ ("stamina: ", STAMINA)
-		pretty_time()
-		counterFps=0
-		inicio_segundo=true	
 	
+
+#funcion _status_control_end_
+func _status_control():
+	if inicio_segundo:
+			
+		if (HEALTH <= 0):
+			DEATH = true
+		
+		if TIRED && !input.hide():
+			stamina(1)
+
+		if (STAMINA <= 0):
+			TIRED = true
+			heal(-1)
+
+		if (STAMINA < MAX_STAMINA && STAMINA > 0 && !input.hide()):
+			TIRED = false
+			_logger_ ("regenerating stamina", STAMINA_REGEN)
+			stamina(STAMINA_REGEN)
+
+		if (HEALTH < MAX_HEALTH && HEALTH > 0 && !TIRED):
+			_logger_ ("regenerating health", HEALTH_REGEN)
+			heal(HEALTH_REGEN)
+		_logger_ ("TIRED stamina", TIRED)
+
+#funcion que aplica el movimiento introducido (por _controls_loop()) y normalizado a la constante SPEED
+func _movement_loop():
+	if DEATH==false:
+		var linear_velocity
+		var floor_normal = Vector2(0,0)
+
+		if is_on_wall():
+			hurt(10)
+		if input.hide():
+			stamina(-1)
+			linear_velocity = movedir.normalized() * HIDE_SPEED
+		if TIRED==true:
+			linear_velocity = movedir.normalized() * TIRED_SPEED
+		if input.hide() == false && TIRED == false:			
+			linear_velocity = movedir.normalized() * SPEED	
+		move_and_slide(linear_velocity, floor_normal)
+		
+#funcion que altera el movimiento en funcion de los inputs de teclado: proyecto/ajustes/mapa_de_entrada
+func _controls_loop():
+	movement()
+	#TODO: attak()
+
+#funcion que checkea si movedir se mueve y no es infinito para actualizar la posicion del spirte
+func _spritedir_loop():
+	if movedir != Vector2.ZERO && movedir != Vector2.INF:
+		spritedir = movedir
+
 #funcion que cambia la animacion en funcion del entorno
 func _animloader_loop(delta):
 	if movedir != Vector2.ZERO:
@@ -87,105 +127,87 @@ func _animloader_loop(delta):
 	else:
 		anim_switch("Idle")
 
-#funcion que altera el movimiento en funcion de los inputs de teclado: proyecto/ajustes/mapa_de_entrada
-func _controls_loop():
-	movement()
-	#TODO: attak()
 
-#funcion que aplica el movimiento introducido (por _controls_loop()) y normalizado a la constante SPEED
-func _movement_loop():
-	if DEATH==false:
-		var linear_velocity
-		var floor_normal = Vector2(0,0)
 
-		if is_on_wall():
-			hurt(10)
-		else:
-			hurt(0)
-		if input.hide():
-			tired(1)
-			linear_velocity = movedir.normalized() * HIDE_SPEED
-		if tired==true:
-			hurt(1)
-			linear_velocity = movedir.normalized() * TIRED_SPEED
-		if input.hide() == false && tired == false:
-			tired(0)
-			hurt(0)
-			linear_velocity = movedir.normalized() * SPEED	
-		move_and_slide(linear_velocity, floor_normal)
+
+#######################################################
+################## FUNCTIONS ##########################
+#######################################################
+
+#funcion tired
+func stamina(stamina):
+	if inicio_segundo:
+		STAMINA += stamina
+		if (STAMINA <= 0):
+			STAMINA = 0;
+		elif STAMINA > 100:
+			STAMINA = 100
+		return STAMINA
+	else:
+		return false
+
+#funcion hurt
+func hurt(damage):
+	if  inicio_segundo && damage != 0 && damage > 0:
+		# TODO modificadores del daño
+		heal (- damage)
+		return damage
+	else:		
+		return false
 		
-		
+#funcion hurt
+func heal(heal):
+	if inicio_segundo:
+		HEALTH += heal
+		if (HEALTH <= 0):
+			HEALTH = 0
+		elif HEALTH > 100:
+			HEALTH = 100
+		return HEALTH
 
-#funcion que checkea si movedir se mueve y no es infinito para actualizar la posicion del spirte
-func _spritedir_loop():
-	if movedir != Vector2.ZERO && movedir != Vector2.INF:
-		spritedir = movedir
+#funcion die
+func die():
+	if DEATH == true:
+		set_physics_process(false)
+		#todo: cargar animacion muerte
+		return true
+	return false
 
-#funcion que devueve el estado de stamina en funcion de delta(frames)
-func _staminaRegen_loop():
-	if inicio_segundo && !DEATH && !delay:
-		if STAMINA < MAX_STAMINA && tired==false:
-			_logger_ ("regenerating stamina", STAMINA_REGEN)
-			STAMINA += STAMINA_REGEN
-		elif STAMINA > MAX_STAMINA:
-			STAMINA = MAX_STAMINA
-			
-#funcion que devueve el health de stamina en funcion de delta(frames)
-func _healthRegen_loop():
-	if inicio_segundo && !DEATH && !delay:
-		if HEALTH < MAX_HEALTH:
-			_logger_ ("regenerating health",HEALTH_REGEN)
-			HEALTH += HEALTH_REGEN
-			if HEALTH > MAX_HEALTH:
-				HEALTH = MAX_HEALTH
-			
 
-#funcion que devueve el health de stamina en funcion de delta(frames)
+#funcion que describe el movimiento
+func movement():
+	movedir.x = -input.left() + input.right()
+	movedir.y = -input.up() + input.down()
+
+#funcion delay
 func _delay():
 	if delay && inicio_segundo:
 		delay=false
 		_logger_ ("delayStatus= ", delay)
 		
 
-#funcion que devuelve el estado de health en funcion de delta(frames)
-func _healthstate_loop(damage_dealt):
-	if DEATH==false:
-		if HEALTH<=0:
-			_logger_ ("death", true)
-			DEATH=true
-		else:
-			if damage_dealt && !delay:
-				HEALTH-=int(damage_dealt)
-				if HEALTH<=0:
-					HEALTH=0
-				delay=true
-				return HEALTH
 
-func _staminastate_loop(stamina_cost):
-	if DEATH==false:
-		_logger_ ("STAMINA",STAMINA)
-		_logger_ ("tired",tired)
-		_logger_ ("stamina_cost",stamina_cost)
-		if STAMINA==0:
-			tired=true
-			_logger_ ("tired",true)
-		elif STAMINA==0 && tired==true && stamina_cost==0:
-			_logger_ ("staminastate_tired",false)
-			tired=false
-		else:
-			_logger_ ("staminastate_tired",false)
-			tired=false
-			if stamina_cost && !delay:
-				STAMINA-=int(stamina_cost)
-				if STAMINA<0:
-					STAMINA=0
-				delay=true
-				return STAMINA
-		
+################## UTILS FUNCTIONS ##########################
 
+#funcion tiempo
+func _time():
+	if counterFps == fps:
+		_logger_ ("health: ", HEALTH)
+		_logger_ ("stamina: ", STAMINA)
+		pretty_time()
+		counterFps=0
+		inicio_segundo=true	
 
-################## MEMBER FUNCTIONS ##########################
+#funcion pretty_time
+func pretty_time():
+	time_now = OS.get_unix_time()
+	var elapsed = time_now - time_start
+	var minutes = elapsed / 60
+	var seconds = elapsed % 60
+	var str_elapsed = "%02d : %02d" % [minutes, seconds]
+	_logger_("elapsed : ", str_elapsed)
 
+#funcion _logger_
 func _logger_ (texto,valor):
 	logger_text.append(texto)
 	if valor !=null:
@@ -193,6 +215,7 @@ func _logger_ (texto,valor):
 	else:
 		logger_value.append("")
 
+#funcion _consolator_printer
 func _consolator_printer ():
 	if inicio_segundo:
 		_logger_("--------------------------", null)
@@ -201,14 +224,7 @@ func _consolator_printer ():
 	logger_text.clear()
 	logger_value.clear()
 
-#funcion tiempo
-func pretty_time():
-	time_now = OS.get_unix_time()
-	var elapsed = time_now - time_start
-	var minutes = elapsed / 60
-	var seconds = elapsed % 60
-	var str_elapsed = "%02d : %02d" % [minutes, seconds]
-	_logger_("elapsed : ", str_elapsed)
+
 
 #funcion que devuelve la direccion en string
 func action_to_String(direction:Vector2) -> String:
@@ -247,37 +263,6 @@ class input:
 		return Input.is_action_just_pressed("ui_attack1")
 	static func hide():
 		return Input.is_action_pressed("ui_hide")
-
-#funcion que describe el movimiento
-func movement():
-	movedir.x = -input.left() + input.right()
-	movedir.y = -input.up() + input.down()
-
-func tired(stamina):
-	if !delay && stamina!=0:
-		stamina_cost=stamina
-		delay_time=TIME
-		return stamina_cost
-	else:
-		stamina_cost=0
-		return false
-
-
-func hurt(damage):
-	if damage!=0 && !delay:
-		damage_dealt=damage
-		delay_time=TIME
-		return damage_dealt
-	else:
-		damage_dealt=0
-		return false
-	
-func die():
-	if DEATH==true:
-		set_physics_process(false)
-		#todo: cargar animacion muerte
-		return true
-	return false
 
 func _on_SwordHit_area_entered(area):
 	if area.is_in_group("hurtbox"):
